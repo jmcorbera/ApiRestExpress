b7a91f53e5f9a34dc90453b9cd25b5354624127e
jcorberaCP <jcorbera@credipaz.com>
2022-04-08 16:11:24 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const morgan=require('morgan');
+
+//settings
+app.set('port', process.env.PORT || 3001);
+app.set('json spaces', 2)
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+//routes
+app.get('/', (req, res) => {    
+    res.json(
+        {
+            "Title": "Hello World"
+        }
+    );
+})
+
+//Initializing server, listening...
+app.listen(app.get('port'),()=>{
+    console.log(`Server listening on port ${app.get('port')}`);
+});
+
